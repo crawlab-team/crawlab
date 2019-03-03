25ee4b8d220aa1ad864c95fec2871ce633dcb60e
Marvin Zhang <tikazyq@gmail.com>
2019-03-03 10:57:19 +0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -9,10 +9,16 @@ with open('requirements.txt') as f:
 setup(
     name='crawlab',
     version='0.0.1',
-    packages=['db', 'test', 'model', 'tasks', 'utils', 'routes', 'constants'],
     url='https://github.com/tikazyq/crawlab',
+    install_requires=requirements,
     license='BSD',
     author='Marvin Zhang',
     author_email='tikazyq@163.com',
-    description='Celery-based web crawler admin platform for managing distributed web spiders regardless of languages and frameworks.'
+    description='Celery-based web crawler admin platform for managing distributed web spiders regardless of languages and frameworks.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    download_url="https://github.com/tikazyq/crawlab/archive/master.zip",
+    packages=find_packages(),
+    keywords=['celery', 'python', 'webcrawler', 'crawl', 'scrapy', 'admin'],
+    zip_safe=True,
 )
