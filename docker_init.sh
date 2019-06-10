876e4056767788e7ad0a0f4f1820b6f7978d6976
Marvin Zhang <tikazyq@gmail.com>
2019-06-11 00:45:13 +0800

    updated Dockerfile

--- docker_init.sh
+++ docker_init.sh
@@ -2,6 +2,6 @@
 cd /opt/crawlab/frontend \
 	&& npm run build:prod \
 	&& mongod --fork --logpath /var/log/mongod.log \
-	&& service nginx reload \
+	&& service nginx start \
 	&& service redis-server start \
 	&& python3 $WORK_DIR/manage.py $*
