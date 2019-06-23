fc15fa42ac3f8fff56bd95ece6a1a462f0ee7612
Marvin Zhang <tikazyq@gmail.com>
2019-06-23 11:42:33 +0800

    added xueqiu spider

---
+++ spiders/xueqiu/xueqiu/pipelines.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import os
+
+from pymongo import MongoClient
+
+
+
+
+class XueqiuPipeline(object):
+    mongo = MongoClient(
+        host='localhost',
+        port=27017
+    )
+    db = mongo['crawlab_test']
+    col = db.get_collection(os.environ.get('CRAWLAB_COLLECTION') or 'results_xueqiu')
+
+    def process_item(self, item, spider):
+        item['task_id'] = os.environ.get('CRAWLAB_TASK_ID')
+        item['_id'] = item['id']
+        if self.col.find_one({'_id': item['_id']}) is None:
+            self.col.save(item)
+        return item
