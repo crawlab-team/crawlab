f06e95537e69707c96911a4b649f4ace1c212905
Marvin Zhang <tikazyq@gmail.com>
2019-07-02 21:01:37 +0800

    added sinastock spider

---
+++ spiders/sinastock/sinastock/items.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class NewsItem(scrapy.Item):
+    # define the fields for your item here like:
+    _id = scrapy.Field()
+    title = scrapy.Field()
+    ts_str = scrapy.Field()
+    ts = scrapy.Field()
+    url = scrapy.Field()
+    text = scrapy.Field()
+    task_id = scrapy.Field()
---
+++ spiders/sinastock/sinastock/pipelines.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import os
+
+from pymongo import MongoClient
+
+
+class SinastockPipeline(object):
+    mongo = MongoClient(
+        host=os.environ.get('MONGO_HOST') or 'localhost',
+        port=int(os.environ.get('MONGO_PORT') or 27017)
+    )
+    db = mongo[os.environ.get('MONGO_DB') or 'crawlab_test']
+    col = db.get_collection(os.environ.get('CRAWLAB_COLLECTION') or 'stock_news')
+
+    def process_item(self, item, spider):
+        item['task_id'] = os.environ.get('CRAWLAB_TASK_ID')
+        item['_id'] = item['url']
+        if self.col.find_one({'_id': item['_id']}) is None:
+            self.col.save(item)
+            return item
