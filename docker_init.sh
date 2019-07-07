ba704b5a471c5b1faba95ce48596f1c8093b5415
Marvin Zhang <tikazyq@gmail.com>
2019-07-07 16:17:08 +0800

    fixed https://github.com/tikazyq/crawlab/issues/78

--- docker_init.sh
+++ docker_init.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 case $1 in
 	master)
-		cd /opt/crawlab/frontend \
+		cd $WORK_DIR/frontend \
 			&& npm run build:prod \
 			&& service nginx start
 		python $WORK_DIR/crawlab/flower.py >> /opt/crawlab/flower.log 2>&1 &
 		python $WORK_DIR/crawlab/worker.py >> /opt/crawlab/worker.log 2>&1 &
-		python $WORK_DIR/crawlab/app.py
+		cd $WORK_DIR/crawlab \
+			&& gunicorn --log-level=DEBUG -b 0.0.0.0 -w 8 $WORK_DIR/crawlab/app:app
 			;;
 	worker)
 		python $WORK_DIR/crawlab/app.py >> /opt/crawlab/app.log 2>&1 &
