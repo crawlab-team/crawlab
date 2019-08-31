1c0b86428cf87e143f832408d4d1982954328b1f
yaziming <tou_tou@126.com>
2019-09-01 00:36:59 +0800

    1. Fix Some I18n Warning. 2. Fix Navigation Duplicated Error When Current Route Path is `/login`. 3. Fix  Parent Level Named Route Warning. 4. Change `request` function From Promise Syntax To Await/Async.

--- frontend/src/api/request.js
+++ frontend/src/api/request.js
@@ -3,13 +3,13 @@ import router from '../router'
 
 let baseUrl = process.env.VUE_APP_BASE_URL ? process.env.VUE_APP_BASE_URL : 'http://localhost:8000'
 
-const request = (method, path, params, data, others = {}) => {
-  return new Promise((resolve, reject) => {
+const request = async (method, path, params, data, others = {}) => {
+  try {
     const url = baseUrl + path
     const headers = {
       'Authorization': window.localStorage.getItem('token')
     }
-    axios({
+    const response = await axios({
       method,
       url,
       params,
@@ -17,15 +17,37 @@ const request = (method, path, params, data, others = {}) => {
       headers,
       ...others
     })
-      .then(resolve)
-      .catch(error => {
-        console.log(error)
-        if (error.response.status === 401) {
-          router.push('/login')
-        }
-        reject(error)
-      })
-  })
+    // console.log(response)
+    return response
+  } catch (e) {
+    if (e.response.status === 401 && router.currentRoute.path !== '/login') {
+      router.push('/login')
+    }
+    await Promise.reject(e)
+  }
+
+  // return new Promise((resolve, reject) => {
+  //   const url = baseUrl + path
+  //   const headers = {
+  //     'Authorization': window.localStorage.getItem('token')
+  //   }
+  //   axios({
+  //     method,
+  //     url,
+  //     params,
+  //     data,
+  //     headers,
+  //     ...others
+  //   })
+  //     .then(resolve)
+  //     .catch(error => {
+  //       console.log(error)
+  //       if (error.response.status === 401) {
+  //         router.push('/login')
+  //       }
+  //       reject(error)
+  //     })
+  // })
 }
 
 const get = (path, params) => {
