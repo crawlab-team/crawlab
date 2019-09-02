71c83251e5186ef5f6801411b4f63113bb22f6e4
yaziming <Shao.Jiang-DAJ@yumchina.com>
2019-09-03 01:16:36 +0800

    feat(backend): add some feat for error message system
    errors support custom http code and support return invalid validated
    error

--- backend/constants/errors.go
+++ backend/constants/errors.go
@@ -1,8 +1,13 @@
 package constants
 
-import "crawlab/errors"
+import (
+	"crawlab/errors"
+	"net/http"
+)
 
 var (
+	ErrorMongoError = errors.NewSystemOPError(1001, "system error:[mongo]%s", http.StatusInternalServerError)
 	//users
-	ErrorUserNotFound = errors.NewBusinessError(10001, "user not found.")
+	ErrorUserNotFound              = errors.NewBusinessError(10001, "user not found.", http.StatusUnauthorized)
+	ErrorUsernameOrPasswordInvalid = errors.NewBusinessError(11001, "username or password invalid", http.StatusUnauthorized)
 )
