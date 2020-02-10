8df923fd3cee934ec12e34f2541fc427fd798e60
marvzhang <tikazyq@163.com>
2020-02-10 17:45:47 +0800

    updated install-nodejs.sh

--- backend/scripts/install-nodejs.sh
+++ backend/scripts/install-nodejs.sh
@@ -19,22 +19,14 @@ export PATH=$NODE_PATH:$PATH
 
 # install chromium
 # See https://crbug.com/795759
-RUN apt-get update && apt-get install -yq libgconf-2-4
+apt-get update && apt-get install -yq libgconf-2-4
 
 # Install latest chrome dev package and fonts to support major 
 # charsets (Chinese, Japanese, Arabic, Hebrew, Thai and a few others)
 # Note: this installs the necessary libs to make the bundled version 
 # of Chromium that Puppeteer
 # installs, work.
-RUN apt-get update && apt-get install -y wget --no-install-recommends \
-    && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
-    && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
-    && apt-get update \
-    && apt-get install -y google-chrome-unstable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst ttf-freefont \
-      --no-install-recommends \
-    && rm -rf /var/lib/apt/lists/* \
-    && apt-get purge --auto-remove -y curl \
-    && rm -rf /src/*.deb
+apt-get update && apt-get install -y --no-install-recommends gconf-service libasound2 libatk1.0-0 libatk-bridge2.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
 
 # install default dependencies
 PUPPETEER_DOWNLOAD_HOST=https://npm.taobao.org/mirrors
