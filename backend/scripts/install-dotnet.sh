e96162ec43ecfa9827c30f65cbc9cdb070548927
marvzhang <tikazyq@163.com>
2020-03-10 17:32:58 +0800

    修复安装脚本

--- backend/scripts/install-dotnet.sh
+++ backend/scripts/install-dotnet.sh
@@ -4,13 +4,13 @@ touch /tmp/install.lock
 # lock
 touch /tmp/install-dotnet.lock
 
-apt-get install curl
+apt-get install -y curl
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-artful-prod artful main" > /etc/apt/sources.list.d/dotnetdev.list'
-apt-get install apt-transport-https
-apt-get update
-apt-get install dotnet-sdk-2.1
+apt-get install -y apt-transport-https
+apt-get update -y
+apt-get install -y dotnet-sdk-2.1
 
 # unlock global
 rm /tmp/install.lock
