ec81344e20964c4f971e9fc77b0c81d1d7e1593a
marvzhang <tikazyq@163.com>
2020-07-30 14:54:07 +0800

    updated install-chromedriver.sh

--- backend/scripts/install-chromedriver.sh
+++ backend/scripts/install-chromedriver.sh
@@ -14,7 +14,6 @@ export DEBIAN_FRONTEND=noninteractive \
   && apt install --no-install-recommends --no-install-suggests -y \
     /tmp/chrome.deb \
   && CHROMIUM_FLAGS='--no-sandbox --disable-dev-shm-usage' \
-  # Patch Chrome launch script and append CHROMIUM_FLAGS to the last line:
   && sed -i '${s/$/'" $CHROMIUM_FLAGS"'/}' /opt/google/chrome/google-chrome \
   && BASE_URL=https://chromedriver.storage.googleapis.com \
   && VERSION=$(curl -sL "$BASE_URL/LATEST_RELEASE") \
