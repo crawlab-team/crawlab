342f9d2e1b3af24991087ed606a8962ea1e4e9f5
marvzhang <tikazyq@163.com>
2021-07-21 16:39:40 +0800

    updated title

---
+++ frontend/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.ts',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Crawlab | Distributed Web Crawler Admin Platform',
+    }
+  }
+}
