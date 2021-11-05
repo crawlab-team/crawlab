b488c39e3a2f349d2f28836c538e803ff02d161f
Marvin Zhang <tikazyq@163.com>
2021-11-05 21:51:40 +0800

    updated env files to allow configuring api base url

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,8 @@
 import 'crawlab-ui/dist/crawlab-ui.css';
 import {createApp} from 'crawlab-ui';
 
-createApp();
+(async function () {
+  // @ts-ignore
+  window.VUE_APP_API_BASE_URL = process.env.VUE_APP_API_BASE_URL;
+  await createApp();
+})();
