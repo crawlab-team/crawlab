ed654ff504fcbf7aaacb49f82dd52f987e1569d4
Marvin Zhang <tikazyq@163.com>
2022-05-16 21:16:33 +0800

    updated docker init

--- bin/docker-start-master.sh
+++ bin/docker-start-master.sh
@@ -4,17 +4,23 @@ then
 	:
 else
 	indexpath=/app/dist/index.html
-	if [[ -v CRAWLAB_BASE_URL ]]; then
+	if test -z "$CRAWLAB_BASE_URL"; then
+		:
+	else
 		sed -i "s?/js/?${CRAWLAB_BASE_URL}/js/?g" ${indexpath}
 		sed -i "s?/css/?${CRAWLAB_BASE_URL}/css/?g" ${indexpath}
 		sed -i "s/  <link rel=\"icon\" type=\"image\/x-icon\" href=\"/  <link rel=\"icon\" type=\"image\/x-icon\" href=\"\/${CRAWLAB_BASE_URL}/g"  ${indexpath}
 		sed -i "s/  <link rel=\"stylesheet\" href=\"/  <link rel=\"stylesheet\" href=\"${CRAWLAB_BASE_URL}\//g"  ${indexpath}
 		sed -i "s/  window.VUE_APP_API_BASE_URL = '/  window.VUE_APP_API_BASE_URL = '\/${CRAWLAB_BASE_URL}/g" ${indexpath}
 	fi
-	if [[ -v CRAWLAB_INIT_BAIDU_TONGJI ]]; then
+	if test -z "$CRAWLAB_INIT_BAIDU_TONGJI"; then
+		:
+	else
 		sed -i "s/  window.VUE_APP_INIT_BAIDU_TONGJI =/  window.VUE_APP_INIT_BAIDU_TONGJI = '${CRAWLAB_INIT_BAIDU_TONGJI}'/g" ${indexpath}
 	fi
-	if [[ -v CRAWLAB_INIT_UMENG ]]; then
+	if test -z "$CRAWLAB_INIT_UMENG"; then
+		:
+	else
 		sed -i "s/  window.VUE_APP_INIT_UMENG =/  window.VUE_APP_INIT_UMENG = '${CRAWLAB_INIT_UMENG}'/g" ${indexpath}
 	fi
 fi
