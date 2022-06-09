11961c2999c00bd74a52c97cf9d88b762230d28e
Marvin Zhang <tikazyq@163.com>
2022-06-09 21:24:34 +0800

    updated go.mod

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,4 @@ module crawlab
 
 go 1.16
 
-require github.com/crawlab-team/crawlab-core v0.6.0-7
+require github.com/crawlab-team/crawlab-core v0.6.1-0.20220609124710-a6fcaa2e7ea6
--- bin/docker-init.sh
+++ bin/docker-init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ "${CRAWLAB_NODE_MASTER}" = "Y" ]; then
-	# start master
-	/bin/bash /app/bin/docker-start-master.sh
+  # start master
+  /bin/bash /app/bin/docker-start-master.sh
 
-	# start crawlab
-	crawlab-server master
+  # start crawlab
+  crawlab-server master
 else
-	# start crawlab
-	crawlab-server worker
+  # start crawlab
+  crawlab-server worker
 fi
