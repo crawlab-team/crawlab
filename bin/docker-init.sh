c0303f5fdb9119a7329340c32af1dc93cdf923aa
Marvin Zhang <tikazyq@163.com>
2022-06-11 11:14:12 +0800

    trigger workflow

--- bin/docker-init.sh
+++ bin/docker-init.sh
@@ -4,9 +4,15 @@ if [ "${CRAWLAB_NODE_MASTER}" = "Y" ]; then
   # start master
   /bin/bash /app/bin/docker-start-master.sh
 
+  # env
+  export IS_MASTER=1
+
   # start crawlab
   crawlab-server master
 else
+  # env
+  export IS_MASTER=0
+
   # start crawlab
   crawlab-server worker
 fi
