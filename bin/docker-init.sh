c6ee25491e6dd6120b8fc60364f28e61bf5a22b9
Marvin Zhang <tikazyq@163.com>
2022-06-11 13:02:40 +0800

    updated docker scripts

--- bin/docker-init.sh
+++ bin/docker-init.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
-if [ "${CRAWLAB_NODE_MASTER}" = "Y" ]; then
-  # start master
-  /bin/bash /app/bin/docker-start-master.sh
-
-  # node type
-  echo "node type: master"
-
-  # start crawlab
-  crawlab-server master
-else
-  # node type
-  echo "node type: worker"
-
-  # start crawlab
-  crawlab-server worker
-fi
+crawlab-server server
--- bin/docker-start-master.sh
+++ bin/docker-start-master.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # replace env
-indexpath=/app/dist/index.html
+indexPath=/app/dist/index.html
 if test -z "$CRAWLAB_BASE_URL"; then
   :
 else
-  sed -i "s?/js/?${CRAWLAB_BASE_URL}/js/?g" ${indexpath}
-  sed -i "s?/css/?${CRAWLAB_BASE_URL}/css/?g" ${indexpath}
-  sed -i "s/  <link rel=\"icon\" type=\"image\/x-icon\" href=\"/  <link rel=\"icon\" type=\"image\/x-icon\" href=\"\/${CRAWLAB_BASE_URL}/g" ${indexpath}
-  sed -i "s/  <link rel=\"stylesheet\" href=\"/  <link rel=\"stylesheet\" href=\"${CRAWLAB_BASE_URL}\//g" ${indexpath}
-  sed -i "s/  window.VUE_APP_API_BASE_URL = '/  window.VUE_APP_API_BASE_URL = '\/${CRAWLAB_BASE_URL}/g" ${indexpath}
+  sed -i "s?/js/?${CRAWLAB_BASE_URL}/js/?g" ${indexPath}
+  sed -i "s?/css/?${CRAWLAB_BASE_URL}/css/?g" ${indexPath}
+  sed -i "s/  <link rel=\"icon\" type=\"image\/x-icon\" href=\"/  <link rel=\"icon\" type=\"image\/x-icon\" href=\"\/${CRAWLAB_BASE_URL}/g" ${indexPath}
+  sed -i "s/  <link rel=\"stylesheet\" href=\"/  <link rel=\"stylesheet\" href=\"${CRAWLAB_BASE_URL}\//g" ${indexPath}
+  sed -i "s/  window.VUE_APP_API_BASE_URL = '/  window.VUE_APP_API_BASE_URL = '\/${CRAWLAB_BASE_URL}/g" ${indexPath}
 fi
 if test -z "$CRAWLAB_INIT_BAIDU_TONGJI"; then
   :
 else
-  sed -i "s/  window.VUE_APP_INIT_BAIDU_TONGJI = ''/  window.VUE_APP_INIT_BAIDU_TONGJI = '${CRAWLAB_INIT_BAIDU_TONGJI}'/g" ${indexpath}
+  sed -i "s/  window.VUE_APP_INIT_BAIDU_TONGJI = ''/  window.VUE_APP_INIT_BAIDU_TONGJI = '${CRAWLAB_INIT_BAIDU_TONGJI}'/g" ${indexPath}
 fi
 if test -z "$CRAWLAB_INIT_UMENG"; then
   :
 else
-  sed -i "s/  window.VUE_APP_INIT_UMENG = ''/  window.VUE_APP_INIT_UMENG = '${CRAWLAB_INIT_UMENG}'/g" ${indexpath}
+  sed -i "s/  window.VUE_APP_INIT_UMENG = ''/  window.VUE_APP_INIT_UMENG = '${CRAWLAB_INIT_UMENG}'/g" ${indexPath}
 fi
 
 # start nginx
