13596436cd47c8be525320703aa18e5f29d31f07
Marvin Zhang <tikazyq@163.com>
2024-07-10 18:59:35 +0800

    refactor: update viper config key for Pro edition check

--- core/utils/system.go
+++ core/utils/system.go
@@ -3,5 +3,5 @@ package utils
 import "github.com/spf13/viper"
 
 func IsPro() bool {
-	return viper.GetString("info.edition") == "global.edition.pro"
+	return viper.GetString("edition") == "global.edition.pro"
 }
