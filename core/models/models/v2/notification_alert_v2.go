5b67717ebe51f18785697f4e4d307b447537ce6c
Marvin Zhang <tikazyq@163.com>
2024-07-30 15:46:02 +0800

    ✨ feat(models): add HasMetricTarget and LastingSeconds fields to NotificationAlertV2

--- core/models/models/v2/notification_alert_v2.go
+++ core/models/models/v2/notification_alert_v2.go
@@ -8,9 +8,11 @@ type NotificationAlertV2 struct {
 	Name                             string             `json:"name" bson:"name"`
 	Description                      string             `json:"description" bson:"description"`
 	Enabled                          bool               `json:"enabled" bson:"enabled"`
+	HasMetricTarget                  bool               `json:"has_metric_target" bson:"has_metric_target"`
 	MetricTargetId                   primitive.ObjectID `json:"metric_target_id,omitempty" bson:"metric_target_id,omitempty"`
 	MetricName                       string             `json:"metric_name" bson:"metric_name"`
 	Operator                         string             `json:"operator" bson:"operator"`
+	LastingSeconds                   int                `json:"lasting_seconds" bson:"lasting_seconds"`
 	TargetValue                      float32            `json:"target_value" bson:"target_value"`
 	Level                            string             `json:"level" bson:"level"`
 }
