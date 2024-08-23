f1915b982f267fe642f1833716d45bfbbecf39a9
Shinku <17696928+Shinku-Chen@users.noreply.github.com>
2024-08-23 12:07:17 +0800

    Update git.go

--- vcs/git.go
+++ vcs/git.go
@@ -3,7 +3,9 @@ package vcs
 import (
 	"github.com/apex/log"
 	"github.com/crawlab-team/crawlab/trace"
+	"github.com/go-git/go-billy/v5"
 	"github.com/go-git/go-billy/v5/memfs"
+	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
